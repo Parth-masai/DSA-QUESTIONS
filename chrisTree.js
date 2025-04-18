38670311ac75d42560f3c4fc1d5eb4d65c0a8608
PARTHIBAN-A <engineerparthiban.a@gmail.com>
2025-04-18 15:29:34 +0530

    Christmas Tree Pattern

--- chrisTree.js
+++ chrisTree.js
@@ -1,17 +1,23 @@
-function chrisTree(n){
-  for(let i=1; i<=n; i++){
-    let tree = "";
-    for(let j=1; j<=n-i; j++){
+function chrisTree(n){{
+  for(var i=1; i<=n; i++){
+    var tree = "";
+    for(var j=1; j<=n-i; j++){
       tree = tree +" ";
     }
-    for(let j=1; j<=((2*i)-1); j++){
+    for(var j=1; j<=((2*i)-1); j++){
       tree= tree+"*"; 
     }
     console.log(tree)
   }
 }
-  
-chrisTree(6) //we are "using two ladder loops inside one for loop". Till i understand the code i'll remember this logic.
+ var trunk = "";
+  for (var p = 1; p <= (n - 2)+1; p++) {
+    trunk += " ";
+  }
+  trunk += "|";
+  console.log(trunk);  
+}
+chrisTree(10) //we are "using two ladder loops inside one for loop". Till i understand the code i'll remember this logic.
 
 // function chrisTree(n){
 //   for(let i=1; i<=n; i++){
